bb0174dadbc4426ac9d770a14654012fc9853b2a
Alexis Davidson <alexisdavidson00@gmail.com>
2022-07-29 14:25:06 +0200

    Add mumbai network to config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,37 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
+require("dotenv").config();
+const { REACT_APP_API_URL, REACT_APP_PRIVATE_KEY } = process.env;
 
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+     hardhat: {},
+     ropsten: {
+        url: REACT_APP_API_URL,
+        accounts: [`0x${REACT_APP_PRIVATE_KEY}`]
+     },
+     rinkeby: {
+       url: process.env.REACT_APP_API_URL_RINKEBY,
+       accounts: ['0x' + process.env.REACT_APP_PRIVATE_KEY_RINKEBY]
+     },
+     matic: {
+       url: process.env.REACT_APP_API_URL_MATIC,
+       accounts: [process.env.REACT_APP_PRIVATE_KEY_MATIC]
+     },
+     mumbai: {
+       url: process.env.REACT_APP_API_URL_MUMBAI,
+       accounts: [process.env.REACT_APP_PRIVATE_KEY_MUMBAI]
+     }
+  },
   paths: {
     artifacts: "./src/backend/artifacts",
     sources: "./src/backend/contracts",
     cache: "./src/backend/cache",
     tests: "./src/backend/test"
   },
+  etherscan: {
+    apiKey: process.env.REACT_APP_ETHERSCAN_API_KEY
+  }
 };
